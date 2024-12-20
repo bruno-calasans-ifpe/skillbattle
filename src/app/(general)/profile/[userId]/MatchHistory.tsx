c65edabc318288fea1f2ef19e490152002b6406e
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2024-12-20 00:38:39 -0300

    add MatchCards to the tab 'criados' in the MatchHistory component

--- src/app/(general)/profile/[userId]/MatchHistory.tsx
+++ src/app/(general)/profile/[userId]/MatchHistory.tsx
@@ -4,15 +4,6 @@ import Title from "@/components/custom/Title";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Challenge } from "@/types/Challenge";
 import MatchCard from "./MatchCard";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 import { useState } from "react";
 import ItemsPagination from "./ItemsPagination";
 
@@ -22,7 +13,8 @@ type MatchHistoryProps = {
 
 export default function MatchHistory({ challenges }: MatchHistoryProps) {
   const itemsPerPage = 5;
-  const [renderedChallenges, setRenderedChallenges] = useState<Challenge[]>([]);
+  const [doneChallenges, setDoneChallenges] = useState<Challenge[]>([]);
+  const [createdChallenges, setCreatedChallenges] = useState<Challenge[]>([]);
 
   return (
     <div className="mb-10">
@@ -33,20 +25,29 @@ export default function MatchHistory({ challenges }: MatchHistoryProps) {
             Realizados({challenges.length})
           </TabsTrigger>
           <TabsTrigger value="created" className="flex-1">
-            Criados(2)
+            Criados({challenges.length})
           </TabsTrigger>
         </TabsList>
         <TabsContent value="done" className="flex flex-col gap-2">
-          {renderedChallenges.map((challenge, index) => (
+          {doneChallenges.map((challenge, index) => (
             <MatchCard key={index} challenge={challenge} />
           ))}
           <ItemsPagination
-            onPageChange={setRenderedChallenges}
+            onPageChange={setDoneChallenges}
             items={challenges}
             itemsPerPage={itemsPerPage}
           />
         </TabsContent>
-        <TabsContent value="created">Change your password here.</TabsContent>
+        <TabsContent value="created" className="flex flex-col gap-2">
+          {createdChallenges.slice(0, 5).map((challenge, index) => (
+            <MatchCard key={index} challenge={challenge} />
+          ))}
+          <ItemsPagination
+            onPageChange={setCreatedChallenges}
+            items={challenges}
+            itemsPerPage={itemsPerPage}
+          />
+        </TabsContent>
       </Tabs>
     </div>
   );
